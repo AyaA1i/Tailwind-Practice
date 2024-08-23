a75ab86780e68469729efe5e4bd8aa1a9073c2d4
ayaaalli <ayaali95248@gmail.com>
2024-08-23 14:52:55 +0300

    percentage

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 w-96 h-96 m-auto mt-10 pt-10">
+        <div className="bg-white w-1/2 h-1/2 m-auto pt-10">
+          <div className="bg-slate-700 w-1/2 h-1/2 m-auto">
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
